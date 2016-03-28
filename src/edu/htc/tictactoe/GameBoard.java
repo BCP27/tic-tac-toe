d318ba6b457953fcaebf3d623474b0f68195a352
BCP27 <gamemeister27@gmail.com>
2016-03-28 18:55:53 -0500

    saving progress

--- src/edu/htc/tictactoe/GameBoard.java
+++ src/edu/htc/tictactoe/GameBoard.java
@@ -85,8 +85,6 @@ public class GameBoard {
                 return true;
             }
         }
-        if (getOpenSquares().length == 0 )
-            return true;
         return false;
     }
 }
--- src/edu/htc/tictactoe/TicTacToe.java
+++ src/edu/htc/tictactoe/TicTacToe.java
@@ -4,34 +4,72 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    char board[] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+    char board[] = {'1','2','3','4','5','6','7','8','9'};
     GameBoard Game = new GameBoard(board);
     Scanner scanner = new Scanner(System.in);
+    Player player1 = new Player("Brian", 'X');
+    Player player2 = new Player("Player2", 'O');
+    public void playerVictory(int playerNumber)
+    {
+        if (playerNumber == 1)
+        {
+            Game.display();
+            player1.addWin();
+            System.out.println(player1.getName() + " wins!");
+            System.out.println(player1.getName() + ": " + player1.getWinCount());
+            System.out.println(player2.getName() + ": " + player2.getWinCount());
+        }
+        if (playerNumber == 2)
+        {
+            player2.addWin();
+            System.out.println(player2.getName() + " wins!");
+            System.out.println(player2.getName() + ": " + player2.getWinCount());
+            System.out.println(player1.getName() + ": " + player1.getWinCount());
+        }
+    }
   public void playGame()
   {
-      Game.display();
-      Player player1 = new Player("Brian", 'X');
-      Player player2 = new Player("Player2", 'O');
+      int turn = 0;
       System.out.println("Player 1 Name: " + player1.getName());
       System.out.println("Player 1 Marker: " + player1.getMarker());
       System.out.println("Player 1 Win Count: " + player1.getWinCount());
       System.out.println("Player 2 Name: " + player2.getName());
       System.out.println("Player 2 Marker: " + player2.getMarker());
       System.out.println("Player 2 Win Count: " + player2.getWinCount());
+
       while (1 == 1)
       {
           Game.display();
+          System.out.println(player1.getName() + "'s turn, their marker is " + player1.getMarker());
+          turn = turn + 1;
           Game.updateSquare(player1.getMove(), 'X');
-          if (Game.isGameWon() == true)
+          if (Game.isGameWon())
+          {
+              playerVictory(1);
+              break;
+          }
+          if (Game.getOpenSquares().length == 0)
           {
               Game.display();
+              System.out.println("Tie Game!");
               break;
           }
           Game.display();
+          System.out.println(player2.getName() + "'s turn, their marker is " + player2.getMarker());
+          turn = turn + 1;
           Game.updateSquare(player2.getMove(), 'O');
-          if (Game.isGameWon() == true)
+          if (Game.isGameWon())
           {
               Game.display();
+              playerVictory(2);
+              break;
+          }
+          if (Game.getOpenSquares().length == 0)
+          {
+              Game.display();
+              System.out.println("Tie Game!");
+              System.out.println(player1.getName() + ": " + player1.getWinCount());
+              System.out.println(player2.getName() + ": " + player2.getWinCount());
               break;
           }
       }
